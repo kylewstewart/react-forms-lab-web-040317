40ec8a89f8545bcd694cf1fb284b9ccb3edb18d6
kylewstewart <kyle.w.stewart@mac.com>
2017-06-07 09:34:46 -0400

    still working

--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -1,18 +1,40 @@
 import React from 'react';
 
 export default class TwitterMessage extends React.Component {
+
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+       value: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this)
   }
+
+  // handleChange = event => {
+  //     this.setState({
+  //       value: event.target.value,
+  //     });
+  //   }
+
+  handleChange(event) {
+      this.setState({
+        value: event.target.value,
+      });
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
       </div>
     );
   }
+
 }
